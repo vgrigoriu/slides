28a9ebefede859747b8c3d67745f2609f5b1d0c7
Victor Grigoriu <vgrigoriu@gmail.com>
2015-07-20 22:07:27 +0300

    Update build.cmd

--- debugging/build.cmd
+++ debugging/build.cmd
@@ -1,1 +1,5 @@
-pandoc -t revealjs -s .\debugging.markdown -o reveal.html -i -V revealjs-url=reveal.js-3.1.0 -V theme=solarized -V transition=linear -V center=false
+@echo off
+
+pandoc -t revealjs -s .\debugging.markdown -o debugging.html -i -V revealjs-url=reveal.js-3.1.0 -V theme=solarized -V transition=linear -V center=false
+
+debugging.html
